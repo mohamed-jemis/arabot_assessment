c7ddacf5f9f91b0827de75330677eeb37f000bd0
mohamed-jemis <avatarmohamed@hotmail.com>
2023-11-20 04:36:07 +0200

    Added Comments

--- Model.py
+++ Model.py
@@ -4,26 +4,38 @@ import tensorflow as tf
 
 
 def load_model():
+    """
+    Loading the pre trained model for question answering from the hugging face transformers library
+    """
     model_name = "bert-large-uncased-whole-word-masking-finetuned-squad"
     tokenizer = BertTokenizer.from_pretrained(model_name)
     model = pipeline('question-answering', model=model_name)
     return model, tokenizer
 
 
-def generate_answer(qa_model, tokenizer, context, question):
-    inputs = tokenizer(question, context, return_tensors="tf")
-    outputs = qa_model(inputs)
+def generate_answer(model, model_tokenizer, _context, _question):
+    """
+    Generating an answer given the context and the question
+
+    Arguments :
+    model-> provide the pretrained model
+    model_tokenizer -> provide the tokenizer loaded
+    _context -> the context needed for the generation
+    _question -> provide the question needed to be fed to the model
+    """
+    inputs = model_tokenizer(_question, _context, return_tensors="tf")
+    outputs = model(inputs)
     start_scores = outputs.start_logits
     end_scores = outputs.end_logits
 
     answer_start = tf.argmax(start_scores, axis=1).numpy()[0]
     answer_end = tf.argmax(end_scores, axis=1).numpy()[0] + 1
 
-    answer = tokenizer.convert_tokens_to_string(
+    generated_answer = tokenizer.convert_tokens_to_string(
         tokenizer.convert_ids_to_tokens(inputs["input_ids"].numpy()[0][answer_start:answer_end]))
-    return answer
+    return generated_answer
 
-
+# Testing the Model
 if __name__ == "__main__":
     # Download the SQuAD dataset
     squad_dataset = load_dataset("squad")
@@ -34,7 +46,7 @@ if __name__ == "__main__":
     # Load the DistilBERT QA model
     qa_model, tokenizer = load_model()
 
-    # Evaluate on the first 100 records
+    # testing the model on the first 100 records
     correct_predictions = 0
     total_predictions = 0
 
@@ -51,7 +63,6 @@ if __name__ == "__main__":
         print("Predicted Answer:", predicted_answer)
         print("="*50)
 
-        # Evaluate accuracy
         total_predictions += 1
         if predicted_answer == answer:
             correct_predictions += 1
--- main.py
+++ main.py
@@ -6,16 +6,25 @@ app = FastAPI()
 
 
 class QARequest(BaseModel):
+    """
+    The Question answering request
+    """
     context: str
     question: str
 
 
 class DatasetRequest(BaseModel):
+    """
+    The DatasetRequest
+    """
     data: list
 
 
 @app.post("/qa")
 async def qa_endpoint(request: QARequest):
+    """
+    Question Answering end point responsible for returning a generated answer in request
+    """
     try:
         qa_model, tokenizer = load_model()
         answer = generate_answer(qa_model, tokenizer, request.context, request.question)
@@ -26,6 +35,9 @@ async def qa_endpoint(request: QARequest):
 
 @app.post("/dataset")
 async def dataset_endpoint(request: DatasetRequest):
+    """
+    Responsible for the Dataset saving in a CSV format
+    """
     try:
         data = request.data
         results = []
